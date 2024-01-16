f9762c7cb74c27ff90b86255a88f1b9fce5f8ec8
melvingcramer911 <davidbr@davidbr.com>
2024-01-16 15:57:02 -0500

    Added-boot-time-to-reboot

--- bash/reboot-win.sh
+++ bash/reboot-win.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
 
-ansible-playbook -i /home/build/ansible-playbooks/inventories/dev/hosts.ini  /home/build/ansible-playbooks/reboot-win.yml
+time  ansible-playbook -i /home/build/ansible-playbooks/inventories/dev/hosts.ini  /home/build/ansible-playbooks/reboot-win.yml
+
+# first run was 1 minute 29 seconds !!
+
